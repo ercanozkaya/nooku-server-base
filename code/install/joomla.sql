42d80b48b0f41cc33dea685cafff22589d026e64
Ercan Ozkaya <ozkayaercan@gmail.com>
2011-06-27 21:41:56 +0300

    Added Installation SQL

---
+++ code/install/joomla.sql
@@ -0,0 +1,350 @@
+#
+# Table structure for table `jos_components`
+#
+
+CREATE TABLE `jos_components` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(50) NOT NULL default '',
+  `link` varchar(255) NOT NULL default '',
+  `menuid` int(11) unsigned NOT NULL default '0',
+  `parent` int(11) unsigned NOT NULL default '0',
+  `admin_menu_link` varchar(255) NOT NULL default '',
+  `admin_menu_alt` varchar(255) NOT NULL default '',
+  `option` varchar(50) NOT NULL default '',
+  `ordering` int(11) NOT NULL default '0',
+  `admin_menu_img` varchar(255) NOT NULL default '',
+  `iscore` tinyint(4) NOT NULL default '0',
+  `params` text NOT NULL,
+  `enabled` tinyint(4) NOT NULL default '1',
+  PRIMARY KEY  (`id`),
+  KEY `parent_option` (`parent`, `option`(32))
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+#
+# Dumping data for table `jos_components`
+#
+
+INSERT INTO `jos_components` VALUES (21, 'Configuration Manager', '', 0, 0, '', 'Configuration', 'com_config', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (22, 'Installation Manager', '', 0, 0, '', 'Installer', 'com_installer', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (23, 'Language Manager', '', 0, 0, '', 'Languages', 'com_languages', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (25, 'Menu Editor', '', 0, 0, '', 'Menu Editor', 'com_menus', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (28, 'Modules Manager', '', 0, 0, '', 'Modules', 'com_modules', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (29, 'Plugin Manager', '', 0, 0, '', 'Plugins', 'com_plugins', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (30, 'Template Manager', '', 0, 0, '', 'Templates', 'com_templates', 0, '', 1, 'site=rhuk_milkyway\nadmin=default', 1);
+INSERT INTO `jos_components` VALUES (31, 'User Manager', 'option=com_users', 0, 0, '', 'Users', 'com_users', 0, '', 1, 'allowUserRegistration=1\nnew_usertype=Registered\nuseractivation=1\nfrontend_userparams=1\n\n', 1);
+INSERT INTO `jos_components` VALUES (32, 'Cache Manager', '', 0, 0, '', 'Cache', 'com_cache', 0, '', 1, '', 1);
+INSERT INTO `jos_components` VALUES (33, 'Control Panel', '', 0, 0, '', 'Control Panel', 'com_cpanel', 0, '', 1, '', 1);
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_groups`
+#
+
+# --------------------------------------------------------
+
+CREATE TABLE `jos_groups` (
+  `id` tinyint(3) unsigned NOT NULL default '0',
+  `name` varchar(50) NOT NULL default '',
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+#
+# Dumping data for table `jos_groups`
+#
+
+INSERT INTO `jos_groups` VALUES (0, 'Public');
+INSERT INTO `jos_groups` VALUES (1, 'Registered');
+INSERT INTO `jos_groups` VALUES (2, 'Special');
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_plugins`
+#
+
+CREATE TABLE `jos_plugins` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(100) NOT NULL default '',
+  `element` varchar(100) NOT NULL default '',
+  `folder` varchar(100) NOT NULL default '',
+  `access` tinyint(3) unsigned NOT NULL default '0',
+  `ordering` int(11) NOT NULL default '0',
+  `published` tinyint(3) NOT NULL default '0',
+  `iscore` tinyint(3) NOT NULL default '0',
+  `client_id` tinyint(3) NOT NULL default '0',
+  `checked_out` int(11) unsigned NOT NULL default '0',
+  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `params` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `idx_folder` (`published`,`client_id`,`access`,`folder`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+INSERT INTO `jos_plugins` VALUES (1, 'Authentication - Joomla', 'joomla', 'authentication', 0, 1, 1, 1, 0, 0, '0000-00-00 00:00:00', '');
+INSERT INTO `jos_plugins` VALUES (5, 'User - Joomla!', 'joomla', 'user', 0, 0, 1, 0, 0, 0, '0000-00-00 00:00:00', 'autoregister=1\n\n');
+INSERT INTO `jos_plugins` VALUES (26, 'System - Koowa', 'koowa', 'system', 0, 1, 1, 1, 0, 0, '0000-00-00 00:00:00', '');
+INSERT INTO `jos_plugins` VALUES (27, 'System - SEF','sef','system',0,2,1,0,0,0,'0000-00-00 00:00:00','');
+INSERT INTO `jos_plugins` VALUES (28, 'System - Debug', 'debug', 'system', 0, 3, 1, 0, 0, 0, '0000-00-00 00:00:00', 'queries=1\nmemory=1\nlangauge=1\n\n');
+INSERT INTO `jos_plugins` VALUES (30, 'System - Cache', 'cache', 'system', 0, 5, 0, 1, 0, 0, '0000-00-00 00:00:00', 'browsercache=0\ncachetime=15\n\n');
+
+# Newly added for Nooku Server
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_menu`
+#
+
+CREATE TABLE `jos_menu` (
+  `id` int(11) NOT NULL auto_increment,
+  `menutype` varchar(75) default NULL,
+  `name` varchar(255) default NULL,
+  `alias` varchar(255) NOT NULL default '',
+  `link` text,
+  `type` varchar(50) NOT NULL default '',
+  `published` tinyint(1) NOT NULL default 0,
+  `parent` int(11) unsigned NOT NULL default 0,
+  `componentid` int(11) unsigned NOT NULL default 0,
+  `sublevel` int(11) default 0,
+  `ordering` int(11) default 0,
+  `checked_out` int(11) unsigned NOT NULL default 0,
+  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `pollid` int(11) NOT NULL default 0,
+  `browserNav` tinyint(4) default 0,
+  `access` tinyint(3) unsigned NOT NULL default 0,
+  `utaccess` tinyint(3) unsigned NOT NULL default 0,
+  `params` text NOT NULL,
+  `lft` int(11) unsigned NOT NULL default 0,
+  `rgt` int(11) unsigned NOT NULL default 0,
+  `home` INTEGER(1) UNSIGNED NOT NULL DEFAULT 0,
+  PRIMARY KEY  (`id`),
+  KEY `componentid` (`componentid`,`menutype`,`published`,`access`),
+  KEY `menutype` (`menutype`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+INSERT INTO `jos_menu` VALUES (1, 'mainmenu', 'Home', 'home', 'index.php?option=com_users&view=user', 'component', 1, 0, 31, 0, 1, 0, '0000-00-00 00:00:00', 0, 0, 0, 3, 'welcome_desc=\nallowUserRegistration=\nnew_usertype=\nuseractivation=\nfrontend_userparams=\npage_title=\nshow_page_title=1\npageclass_sfx=\nsecure=0\n\n', 0, 0, 1);
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_menu_types`
+#
+
+CREATE TABLE `jos_menu_types` (
+  `id` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
+  `menutype` VARCHAR(75) NOT NULL DEFAULT '',
+  `title` VARCHAR(255) NOT NULL DEFAULT '',
+  `description` VARCHAR(255) NOT NULL DEFAULT '',
+  PRIMARY KEY(`id`),
+  UNIQUE `menutype`(`menutype`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+INSERT INTO `jos_menu_types` VALUES (1, 'mainmenu', 'Main Menu', 'The main menu for the site');
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_modules`
+#
+
+CREATE TABLE `jos_modules` (
+  `id` int(11) NOT NULL auto_increment,
+  `title` text NOT NULL,
+  `content` text NOT NULL,
+  `ordering` int(11) NOT NULL default '0',
+  `position` varchar(50) default NULL,
+  `checked_out` int(11) unsigned NOT NULL default '0',
+  `checked_out_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `published` tinyint(1) NOT NULL default '0',
+  `module` varchar(50) default NULL,
+  `numnews` int(11) NOT NULL default '0',
+  `access` tinyint(3) unsigned NOT NULL default '0',
+  `showtitle` tinyint(3) unsigned NOT NULL default '1',
+  `params` text NOT NULL,
+  `iscore` tinyint(4) NOT NULL default '0',
+  `client_id` tinyint(4) NOT NULL default '0',
+  `control` TEXT NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `published` (`published`,`access`),
+  KEY `newsfeeds` (`module`,`published`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+INSERT INTO `jos_modules` VALUES (2, 'Login', '', 1, 'login', 0, '0000-00-00 00:00:00', 1, 'mod_login', 0, 0, 1, '', 1, 1, '');
+INSERT INTO `jos_modules` VALUES (8, 'Toolbar','',1,'toolbar',0,'0000-00-00 00:00:00',1,'mod_toolbar',0,2,1,'',1, 1, '');
+INSERT INTO `jos_modules` VALUES (12, 'Admin Menu','', 1,'menu', 0,'0000-00-00 00:00:00', 1,'mod_menu', 0, 2, 1, '', 0, 1, '');
+INSERT INTO `jos_modules` VALUES (13, 'Admin SubMenu','', 1,'submenu', 0,'0000-00-00 00:00:00', 1,'mod_submenu', 0, 2, 1, '', 0, 1, '');
+INSERT INTO `jos_modules` VALUES (14, 'User Status','', 1,'status', 0,'0000-00-00 00:00:00', 1,'mod_status', 0, 2, 1, '', 0, 1, '');
+INSERT INTO `jos_modules` VALUES (15, 'Title','', 1,'title', 0,'0000-00-00 00:00:00', 1,'mod_title', 0, 2, 1, '', 0, 1, '');
+
+CREATE TABLE `jos_modules_menu` (
+  `moduleid` int(11) NOT NULL default '0',
+  `menuid` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`moduleid`,`menuid`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_session`
+#
+
+CREATE TABLE `jos_session` (
+  `username` varchar(150) default '',
+  `time` varchar(14) default '',
+  `session_id` varchar(200) NOT NULL default '0',
+  `guest` tinyint(4) default '1',
+  `userid` int(11) default '0',
+  `usertype` varchar(50) default '',
+  `gid` tinyint(3) unsigned NOT NULL default '0',
+  `client_id` tinyint(3) unsigned NOT NULL default '0',
+  `data` longtext,
+  PRIMARY KEY  (`session_id`(64)),
+  KEY `whosonline` (`guest`,`usertype`),
+  KEY `userid` (`userid`),
+  KEY `time` (`time`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_templates_menu`
+#
+
+CREATE TABLE `jos_templates_menu` (
+  `template` varchar(255) NOT NULL default '',
+  `menuid` int(11) NOT NULL default '0',
+  `client_id` tinyint(4) NOT NULL default '0',
+  PRIMARY KEY (`menuid`, `client_id`, `template`(255))
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# Dumping data for table `jos_templates_menu`
+INSERT INTO `jos_templates_menu` VALUES ('system', '0', '0');
+INSERT INTO `jos_templates_menu` VALUES ('default', '0', '1');
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_users`
+#
+
+CREATE TABLE `jos_users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) NOT NULL default '',
+  `username` varchar(150) NOT NULL default '',
+  `email` varchar(100) NOT NULL default '',
+  `password` varchar(100) NOT NULL default '',
+  `usertype` varchar(25) NOT NULL default '',
+  `block` tinyint(4) NOT NULL default '0',
+  `sendEmail` tinyint(4) default '0',
+  `gid` tinyint(3) unsigned NOT NULL default '1',
+  `registerDate` datetime NOT NULL default '0000-00-00 00:00:00',
+  `lastvisitDate` datetime NOT NULL default '0000-00-00 00:00:00',
+  `activation` varchar(100) NOT NULL default '',
+  `params` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY (`email`),
+  KEY `usertype` (`usertype`),
+  KEY `idx_name` (`name`),
+  KEY `gid_block` (`gid`, `block`),
+  KEY `username` (`username`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_core_acl_aro`
+#
+
+CREATE TABLE `jos_core_acl_aro` (
+  `id` int(11) NOT NULL auto_increment,
+  `section_value` varchar(240) NOT NULL default '0',
+  `value` varchar(240) NOT NULL default '',
+  `order_value` int(11) NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `hidden` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `jos_section_value_value_aro` (`section_value`(100),`value`(100)),
+  KEY `jos_gacl_hidden_aro` (`hidden`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_core_acl_aro_map`
+#
+
+CREATE TABLE  `jos_core_acl_aro_map` (
+  `acl_id` int(11) NOT NULL default '0',
+  `section_value` varchar(230) NOT NULL default '0',
+  `value` varchar(100) NOT NULL,
+  PRIMARY KEY  (`acl_id`,`section_value`,`value`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_core_acl_aro_groups`
+#
+CREATE TABLE `jos_core_acl_aro_groups` (
+  `id` int(11) NOT NULL auto_increment,
+  `parent_id` int(11) NOT NULL default '0',
+  `name` varchar(255) NOT NULL default '',
+  `lft` int(11) NOT NULL default '0',
+  `rgt` int(11) NOT NULL default '0',
+  `value` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`id`),
+  KEY `jos_gacl_parent_id_aro_groups` (`parent_id`),
+  KEY `jos_gacl_lft_rgt_aro_groups` (`lft`,`rgt`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+#
+# Dumping data for table `jos_core_acl_aro_groups`
+#
+
+INSERT INTO `jos_core_acl_aro_groups` VALUES (17,0,'ROOT',1,22,'ROOT');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (28,17,'USERS',2,21,'USERS');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (29,28,'Public Frontend',3,12,'Public Frontend');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (18,29,'Registered',4,11,'Registered');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (19,18,'Author',5,10,'Author');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (20,19,'Editor',6,9,'Editor');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (21,20,'Publisher',7,8,'Publisher');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (30,28,'Public Backend',13,20,'Public Backend');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (23,30,'Manager',14,19,'Manager');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (24,23,'Administrator',15,18,'Administrator');
+INSERT INTO `jos_core_acl_aro_groups` VALUES (25,24,'Super Administrator',16,17,'Super Administrator');
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_core_acl_groups_aro_map`
+#
+CREATE TABLE `jos_core_acl_groups_aro_map` (
+  `group_id` int(11) NOT NULL default '0',
+  `section_value` varchar(240) NOT NULL default '',
+  `aro_id` int(11) NOT NULL default '0',
+  UNIQUE KEY `group_id_aro_id_groups_aro_map` (`group_id`,`section_value`,`aro_id`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+# --------------------------------------------------------
+
+#
+# Table structure for table `jos_core_acl_aro_sections`
+#
+CREATE TABLE `jos_core_acl_aro_sections` (
+  `id` int(11) NOT NULL auto_increment,
+  `value` varchar(230) NOT NULL default '',
+  `order_value` int(11) NOT NULL default '0',
+  `name` varchar(230) NOT NULL default '',
+  `hidden` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `jos_gacl_value_aro_sections` (`value`),
+  KEY `jos_gacl_hidden_aro_sections` (`hidden`)
+) ENGINE=MyISAM CHARACTER SET `utf8`;
+
+INSERT INTO `jos_core_acl_aro_sections` VALUES (10,'users',1,'Users',0);
+
+INSERT INTO `jos_core_acl_aro` VALUES (10, 'users', '62', 0, 'Administrators', 0);
+
+INSERT INTO `jos_core_acl_groups_aro_map` VALUES (25, '', 10);
+
+INSERT INTO `jos_users` VALUES (62, 'Administrators', 'admin', 'admin@admin.com', 'e389d8c7054781b3428c8a08b8d87594:J0Mjx4daffIi91hGGvlXJiwPDvQ8EAMW', 'Super Administrator', 0, 1, 25, '2011-06-21 00:45:11', '2011-06-24 12:15:22', '', 'admin_language=en-GB\nlanguage=en-GB\neditor=none\ntimezone=0\n\n');
